5993901c1bf26fa57bb46694450eb75828c60e15
Ka1sa1 <vincentbaobaoli@gmail.com>
2026-04-05 03:43:01 +0800

    add filter and buttons after search
    need fix bug when null search

---
+++ School.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+public class School {
+    public String schoolId;
+    public String name;
+    public String category;
+    public String level;
+    public String address;
+    public Double latitude;
+    public Double longitude;
+    public String website;
+}
